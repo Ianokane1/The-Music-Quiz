ed233f88a15e10bc137b551718cb557308cdcd80
Ian Kane <ianwilliamkane@gmail.com>
2021-12-18 14:20:24 +0000

    Add feedback button and make changed to CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -89,18 +89,18 @@ var questionBank= [
     }
 ]
 
-var question= document.getElementById('question');
-var quizContainer= document.getElementById('quiz-container');
-var scorecard= document.getElementById('scorecard');
-var option0= document.getElementById('option0');
-var option1= document.getElementById('option1');
-var option2= document.getElementById('option2');
-var option3= document.getElementById('option3');
-var next= document.querySelector('.next');
-var points= document.getElementById('score');
-var span= document.querySelectorAll('span');
-var i=0;
-var score= 0;
+let question= document.getElementById('question');
+let quizContainer= document.getElementById('quiz-container');
+let scorecard= document.getElementById('scorecard');
+let option0= document.getElementById('option0');
+let option1= document.getElementById('option1');
+let option2= document.getElementById('option2');
+let option3= document.getElementById('option3');
+let next= document.querySelector('.next');
+let points= document.getElementById('score');
+let span= document.querySelectorAll('span');
+let i=0;
+let score= 0;
 
 //function to display questions
 function displayQuestion(){
@@ -113,5 +113,54 @@ function displayQuestion(){
     option2.innerHTML= questionBank[i].option[2];
     option3.innerHTML= questionBank[i].option[3];
     stat.innerHTML= "Question"+' '+(i+1)+' '+'of'+' '+questionBank.length;
+}
+
+//function to calculate scores
+function calcScore(e){
+    if(e.innerHTML===questionBank[i].answer && score<questionBank.length)
+    {
+        score= score+1;
+        document.getElementById(e.id).style.background= 'limegreen';
+    }
+    else{
+        document.getElementById(e.id).style.background= 'tomato';
+    }
+    setTimeout(nextQuestion,300);
+}
+
+//function to display next question
+function nextQuestion(){
+    if(i<questionBank.length-1)
+    {
+        i=i+1;
+        displayQuestion();
+    }
+    else{
+        points.innerHTML= score+ '/'+ questionBank.length;
+        quizContainer.style.display= 'none';
+        scoreboard.style.display= 'block'
+    }
+}
+
+//click events to next button
+next.addEventListener('click',nextQuestion);
+
+//Back to Quiz button event
+function backToQuiz(){
+    location.reload();
+}
+
+//function to check Answers
+function checkAnswer(){
+    var answerBank= document.getElementById('answerBank');
+    var answers= document.getElementById('answers');
+    answerBank.style.display= 'block';
+    scoreboard.style.display= 'none';
+    for(var a=0;a<questionBank.length;a++)
+    {
+        var list= document.createElement('li');
+        list.innerHTML= questionBank[a].answer;
+        answers.appendChild(list);
+    }
 }
 displayQuestion();
